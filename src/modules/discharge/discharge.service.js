8bafa8657a299291b78a5ede1736d33f4f1b9cb9
Ryu Siegert <ryu.siegert@gmail.com>
2026-07-19 22:59:33 +0200

    Used validation in discharge.service in startDischarge

--- src/modules/discharge/discharge.service.js
+++ src/modules/discharge/discharge.service.js
@@ -1,23 +1,27 @@
 import { randomUUID } from 'node:crypto'
-import { AppError } from '../../errors/app-error'
-import { type } from 'node:os'
+import { AppError } from '../../errors/app-error.js'
 
 const validateDischargeInput = requestBody => {
-    const patient = requestBody.patient
-    const patientId = patient.patientId
-
   if (!requestBody || typeof requestBody !== 'object' || Array.isArray(requestBody)) {
     throw new AppError(
         400,
         'INVALID_REQUEST_BODY',
         'The RequestBody must be a Json object'
     )
-  } else if (!patient || typeof patient !== 'object' || Array.isArray(patient)) {
+  }
+
+  const patient = requestBody.patient
+
+  if (!patient || typeof patient !== 'object' || Array.isArray(patient)) {
     throw new AppError(
         400,
         'INVALID_PATIENT',
         'The patient is unknown')
-  } else if (typeof patientId !== 'stirng' || patientId.trim() !== '') {
+  }
+  
+  const patientId = patient.patientId
+  
+  if (typeof patientId !== 'string' || patientId.trim() === '') {
     throw new AppError(
         400,
         'INVALID_PATIENT_ID',
@@ -29,9 +33,10 @@ const validateDischargeInput = requestBody => {
 }
 
 export const startDischarge = (requestBody) => {
+    const patientId = validateDischargeInput(requestBody)
     return {
         transactionId: randomUUID(),
         status: "STARTED",
-        patientId: requestBody.patient.patientId
+        patientId
     }
 }
